74d678975e03e2447b7626f9bb6a11841630c1d7
JoseSojo <jsojo346@gmail.com>
2022-12-11 17:28:08 -0400

    primer commit

---
+++ src/router/api.js
@@ -0,0 +1,9 @@
+const express = require('express');
+const router = express.Router();
+const {OnSession,OffSession} = require('../bin/auth.js');
+const Note = require('../schemas/animales.js');
+const Note = require('../schemas/categorias.js');
+
+
+
+module.exports = router;
